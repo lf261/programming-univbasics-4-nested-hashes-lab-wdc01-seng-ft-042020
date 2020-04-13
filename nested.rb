0f1758c7c44755a48e365a2db329bb84d36ddfc6
Luis Fernandez <luis.fernandez2093@gmail.com>
2020-04-13 16:23:31 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -116,9 +116,9 @@ def changing_alan
         :known_for => "Unix",
         :languages => ["C"]
       }
-      
-      :alan_kay[:known_for] = "GUI"
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
   
   programmer_hash
 end
@@ -142,6 +142,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-  :dennis_ritchie[:languages] = ["C" , "Assembly"]
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
   programmer_hash
 end
